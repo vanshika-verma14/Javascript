4150798b59e0ff4808ec2ec5a4ffb3dfe98e9b04
vanshika-verma14 <vanshika9994verma@gmail.com>
2024-11-18 23:53:38 +0530

    js

---
+++ conditional_statement.js
@@ -0,0 +1,14 @@
+// "use strict";
+// const ps = require("prompt-sync")
+// const prompt =ps()
+
+let a = prompt("what is yur age");
+console.log(a)
+if (a<18){
+    alert(" YOU R UNDERAGE")
+}
+// ABOVE THING ONLY RUN IN CONSOLE BROWSER OR REPLIT
+
+// General if else statements like java
+// Switch case
+// ternary operator
