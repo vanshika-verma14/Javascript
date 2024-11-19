27c6d912a31000ff6b424d700d2477f841f53405
vanshika-verma14 <vanshika9994verma@gmail.com>
2024-11-19 23:07:55 +0530

    js

---
+++ do_while.js
@@ -0,0 +1,19 @@
+let i  = 0
+let n = 8
+while(i<n){
+    console.log(i)
+    i++
+}
+console.log("----------------")
+console.log(i)
+console.log("----------------")
+
+i  = 0
+n = 8
+do {
+    console.log(i)
+    ++i 
+}while(i<n)
+console.log("----------------")
+console.log(i)
+console.log("----------------")
